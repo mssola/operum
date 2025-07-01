28399cd9a549cc4edf8c557883c940532821c28d
Miquel Sabaté Solà <mikisabate@gmail.com>
2025-07-01 21:24:47 +0200

    Force english locale for tests
    Signed-off-by: Miquel Sabaté Solà <mikisabate@gmail.com>

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 I18n.available_locales = %i[en ca]
-I18n.default_locale = ENV.fetch('OPERUM_DEFAULT_LOCALE', :ca).to_sym
+
+I18n.default_locale = if Rails.env.test?
+                        :en
+                      else
+                        ENV.fetch('OPERUM_DEFAULT_LOCALE', :ca).to_sym
+                      end
 
 Rails.logger.info "Locale: #{I18n.default_locale}"
